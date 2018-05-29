4ee351c00cd2c08f932d4e694f4a0a3500e03c53
artman41 <artman41@gmail.com>
2018-05-29 02:08:23 +0100

    Updated server hub ip to constantly on hub

--- BeatSaberMultiplayerServer/Misc/Settings.cs
+++ BeatSaberMultiplayerServer/Misc/Settings.cs
@@ -146,7 +146,7 @@ namespace BeatSaberMultiplayerServer.Misc {
                 MarkDirty = markDirty;
                 _port = 3700;
                 _serverName = "New Server";
-                _serverHubIP = "127.0.0.1";
+                _serverHubIP = "beatsaber.jaddie.co.uk";
                 _serverHubPort = 3701;
                 _lobbyTime = 60;
                 SongDirectory = "AvailableSongs/";
