4989584937b61e843fc85e6fcc2f1f7969dc5fa5
andruzzzhka <andruzzzhka@yandex.ru>
2019-01-28 22:47:14 +0700

    Bumped version to 0.6.0.0

--- BeatSaberMultiplayer/Plugin.cs
+++ BeatSaberMultiplayer/Plugin.cs
@@ -12,8 +12,8 @@ namespace BeatSaberMultiplayer
     {
         public string Name => "Beat Saber Multiplayer";
 
-        public string Version => "0.5.5.0";
-        public static uint pluginVersion = 550;
+        public string Version => "0.6.0.0";
+        public static uint pluginVersion = 600;
 
         public static Plugin instance;
 
--- BeatSaberMultiplayer/Properties/AssemblyInfo.cs
+++ BeatSaberMultiplayer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BeatSaberMultiplayer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.5.0")]
-[assembly: AssemblyFileVersion("0.5.5.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
