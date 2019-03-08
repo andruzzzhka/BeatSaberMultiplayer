507332cfe9732edf6e87eab6f7d01fb380179a29
andruzzzhka <andruzzzhka@yandex.ru>
2019-03-08 18:52:16 +0700

    Bumped version to 0.6.1.4;

--- BeatSaberMultiplayer/Properties/AssemblyInfo.cs
+++ BeatSaberMultiplayer/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.1.3")]
-[assembly: AssemblyFileVersion("0.6.1.3")]
+[assembly: AssemblyVersion("0.6.1.4")]
+[assembly: AssemblyFileVersion("0.6.1.4")]
