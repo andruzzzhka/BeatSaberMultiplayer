ecec4dc40d73719d5f2da28f6776e0e339c8219f
andruzzzhka <andruzzzhka@yandex.ru>
2019-06-04 13:19:53 +0700

    Bumped version to 0.6.3.2; Added new hub;

--- BeatSaberMultiplayer/Misc/Config.cs
+++ BeatSaberMultiplayer/Misc/Config.cs
@@ -260,8 +260,8 @@ namespace BeatSaberMultiplayer {
 
         Config()
         {
-            _serverHubIPs = new string[] { "127.0.0.1", "soupwhale.com", "hub.assistant.moe", "hub.n3s.co", "hub.auros.red", "beige.space", "treasurehunters.nz", "beatsaber.networkauditor.org", "hub.ligma.site", "hub.jogi-server.de", "beatsaberhub.freddi.xyz" };
-            _serverHubPorts = new int[] { 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700 };
+            _serverHubIPs = new string[] { "127.0.0.1", "soupwhale.com", "hub.assistant.moe", "hub.n3s.co", "hub.auros.red", "beige.space", "treasurehunters.nz", "beatsaber.networkauditor.org", "hub.ligma.site", "hub.jogi-server.de", "beatsaberhub.freddi.xyz", "hub.just-skill.ru" };
+            _serverHubPorts = new int[] { 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700, 3700 };
             _showAvatarsInGame = false;
             _showOtherPlayersBlocks = false;
             _showAvatarsInRoom = true;
